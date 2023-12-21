121ce1934da6530fbe472d54d0d0240f09b4ef9c
Destanto <destantomy8@gmail.com>
2023-12-21 13:06:32 +0700

    MERNstack-opv 1st commit with auth

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,40 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import {Link} from 'react-router-dom'
+import {useLogout} from '../hooks/useLogout'
+import {useAuthContext} from '../hooks/useAuthContext'
+
+const navbar = () => {
+
+  const {logout} = useLogout()
+  const {user} = useAuthContext()
+
+  const handleClick = () => {
+    logout()
+  }
+
+  return (
+    <header>
+        <div className="container">
+            <Link to='/'>
+                <h1>!semangat DIET app</h1>
+            </Link>
+            <nav>
+              {user && (
+                <div>
+                <span>{user.email}</span>
+                <button onClick={handleClick}>Logout</button>
+              </div>
+              )}
+              {!user && (
+              <div>
+                <Link to={'/login'}>Login</Link>
+                <Link to={'/signup'}>Signup</Link>
+              </div>
+              )}
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default navbar
---
+++ frontend/src/components/workoutDetails.js
@@ -0,0 +1,42 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
+import { useAuthContext } from '../hooks/useAuthContext'
+import formatDistanceToNow from 'date-fns/formatDistanceToNow'
+
+const workoutDetails = ({workout}) => {
+
+  const { dispatch } = useWorkoutsContext()
+  const {user} = useAuthContext()
+
+  const handClick = async () => {
+    if (!user) {
+      return
+  }
+
+    const response = await fetch('/api/workouts/' + workout._id, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Bearer ${user.token}`
+    }
+    })
+    const json = await response.json()
+
+    if (response.ok) {
+      dispatch({type: 'DELETE_WORKOUT', payload: json})
+    }
+  }
+
+  return (
+    <div className='workout-details'>
+        <h3>{workout.title}</h3>
+        <p>Load (kg): {workout.load}</p>
+        <p>Reps: {workout.reps}</p>
+        <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+        {/* <p>{workout.createdAt}</p> */}
+        <span onClick={handClick}>delete</span>
+    </div>
+  )
+}
+
+export default workoutDetails
